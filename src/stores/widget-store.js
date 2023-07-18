125fcadaafc36649c1ed4280d4ceb9fb95b452ab
vprint <valentin.printemps@gmail.com>
2023-07-19 00:20:50 +0200

    Modification du widgetStore
    Ajout de l'état de l'outil dessin

--- src/stores/widget-store.js
+++ src/stores/widget-store.js
@@ -3,13 +3,33 @@ import { defineStore } from 'pinia';
 export const useWidgetStore = defineStore('widget', {
   state: () => ({
     _widget: null,
+    _drawMode: {
+      addLine: false,
+      addPolygon: false,
+      modify: false
+    }
   }),
   getters: {
-    widget: (state) => state._widget
+    widget: (state) => state._widget,
+    drawMode: (state) => {
+      for (let e in state._drawMode) {
+        if (state._drawMode[e]) {
+          return state._drawMode[e]
+        }
+      }
+    },
+    drawModeTest: (state) => state._drawMode,
   },
   actions: {
     setActiveWidget(name) {
       this._widget = name
+    },
+    setDrawMode() {
+      this._drawMode = {
+        addLine: false,
+        addPolygon: false,
+        modify: true
+      }
     },
   },
 });
